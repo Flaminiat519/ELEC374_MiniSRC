0a8d0f3f499aba64e33469af7ac5b55e0a7e75af
Flaminiat519 <flaminiatesta5@gmail.com>
2026-01-20 10:48:00 -0500

    Project Folder Added

---
+++ Quartus Project Folder/bus.v
@@ -0,0 +1,3 @@
+module Bus (
+	//Mux
+	input [7:0]BusMuxInRZ, input [7:0]BusMuxInRA, input  
---
+++ Quartus Project Folder/special_registers.v
@@ -0,0 +1,21 @@
+//Register Module for PC, IR, Y, Z, MAR, HI and LO
+
+module register #(parameter DATA_WIDTH_IN = 32, DATA_WIDTH_OUT = 32, INIT = 8'h0)(//idk what the init is
+	input clear,clock,enable,
+	input [DAYA_WIDTH_IN-1:0]BusMuxOut,
+	output wire [DATA_WIDTH_OUT-1:0]BusMuxIn
+);
+reg [DATA_WIDTH_IN-1:0]q;
+initial q= INIT;
+always @ (posedge clock)
+		begin
+			if (clear) begin
+				q <= {DATA_WIDTH_IN{1'b0}}; // what
+			end
+			else if (enable) begin
+				q <= BusMuxOut;
+			end
+		end
+	assign BusMuxIn = q[DATA_WIDTH_OUT-1:0];
+endmodule
+	
