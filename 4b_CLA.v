b0ee51942af3e3ce84b3225a0c352063a453e668
duncankeitzke4343 <duncan@keitzke.com>
2026-02-01 21:52:02 -0500

    added CLA

---
+++ 4b_CLA.v
@@ -0,0 +1,25 @@
+module 4b_CLA(
+	input wire [3:0] a, b,
+	input wire c_in,
+	output wire [3:0] s,
+	output wire c_out
+);
+
+	wire [3:0] G, P;
+	wire [3:0] carry;
+	wire [3:0] carry_outs; //left blank
+
+	assign G = a & b;
+	assign P = a ^ b;
+
+	assign carry[0] = c_in;
+	assign carry[1] = G[0] | (P[0]&carry[0]);
+	assign carry[2] = G[1] | (P[1]&G[0]) | (P[1]&P[0]&carry[0]);
+	assign carry[3] = G[2] | (P[2]&G[1]) | (P[2]&P[1]&G[0]) | (P[2]&P[1]&P[0]&carry[0]);
+	assign c_out = G[3] | (P[3]&G[2]) | (P[3]&P[2]&G[1]) | (P[3]&P[2]&P[1]&G[0]) | (P[3]&P[2]&P[1]&P[0]&carry[0]);
+
+	full_adder fa0 (a[0], b[0], carry[0], sum[0], dummy[0]);
+	full_adder fa1 (a[1], b[1], carry[1], sum[1], dummy[1]);
+	full_adder fa2 (a[2], b[2], carry[2], sum[2], dummy[2]);
+	full_adder fa3 (a[3], b[3], carry[3], sum[3], dummy[3]);
+endmodule
