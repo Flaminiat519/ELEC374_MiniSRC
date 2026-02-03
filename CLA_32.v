5cf90022e83f24f6bb0cd9c245fbb48a2a878adf
victoriaknightt <22rrkt@queensu.ca>
2026-02-03 09:18:49 -0500

    adding comments to the 32-bit CLA

--- CLA_32.v
+++ CLA_32.v
@@ -1,12 +1,17 @@
+//32-bit carry-lookahead adder
 module CLA_32(
+	//define input and output wires, 32-bits
 	input [31:0] a, b,
 	input wire c_in,
 	input wire [31:0] s,
 	output wire c_out
 );
 
+	//defining intermediate wire
 	wire c_out_temp;
-	
+
+	//using instances of the 16-bit CLA to compute
+	//the relevant outputs
 	CLA_16 adder1 (a[15:0], b[15:0], c_in, s[15:0], c_out_temp);
 	CLA_16 adder2 (a[31:16], b[31:16], c_out_temp, s[31:16], c_out);
 endmodule
