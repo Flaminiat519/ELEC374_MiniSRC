9784ffed91d5700e9753feb7b2a7879fecd0fe1b
duncankeitzke4343 <duncan@keitzke.com>
2026-02-02 20:12:13 -0500

    Finished ALU arithmetic

--- div.v
+++ div.v
@@ -1,53 +1,43 @@
-//Non-Restoring Division Algorithm
-
+// Non-Restoring Division Algorithm
 module div #(parameter n = 32)(
-	input [n - 1:0] dividend, divisor,
-	output reg [n - 1:0] remainder, quotient
+    input  [n - 1:0] dividend, divisor,
+    output reg [n - 1:0] remainder, quotient
 );
 
-reg signed [n:0] A; //remainder
-reg [n-1:0] Q; //quotient
-reg [n-1:0] M;
-reg [n-1:0] Q_mag; //Magnitude of M
+    reg signed [n:0] A;      // remainder
+    reg [n-1:0] Q;           // quotient
+    reg [n-1:0] M;           // divisor
+    integer i;
 
-//Sign of Q and A
-reg sign_Q:
-reg sign_A;
+    always @(*) begin
+        A = 0;               // Set A to 0
+        Q = dividend;
+        M = divisor;
+        
+        for (i = 0; i < n; i = i + 1) begin
+            // Shift A and Q left by 1 bit
+            {A, Q} = {A, Q} << 1;
 
-integer i;
-
-always @(*) begin
-	sign_Q = dividend[n-1] ^ divsor[n-1];
-	sign_r = dividend[n-1];
-	
-	Q_mag = dividend[n-1] ? -dividend : dividend;
-	M = divisor[n-1] ? divisor : divisor;
-	
-	A = 0; //Set A to 0
-	Q = Q_mag;
-	
-	for(i = 0; i < n; i = i + 1) 
-	begin
-		//Shift A and Q by 1 bit
-		{A, Q} = {A, Q} << 1;
-		if(A >= 0) begin //if A is + subtract M
-			A = A - M;
-		end else begin //if A is - add M
-			A = A + M;
-		end
-		
-		if(A >= 0) begin //Change bit of quotient based off sign of A
-			Q[0] = 1'b1;
-		end else begin
-			Q[0] = 1'b0;
-		end
-	end
-	
-	if (A < 0) begin //Restore if A is negative
-		A = A + M;
-	end
-	
-	quotient = sign_Q ? -Q : Q; //Set remainder based off sign
-	remainder = sign_A ? -A[n-1:0] : A[n-1:0];
-end
+            if (A >= 0) begin        // if A is positive, subtract M
+                A = A - M;
+            end else begin           // if A is negative, add M
+                A = A + M;
+            end
+            
+            // Set quotient bit based on sign of A
+            if (A >= 0) begin
+                Q[0] = 1'b1;
+            end else begin
+                Q[0] = 1'b0;
+            end
+        end
+        
+        // Final restore if A is negative
+        if (A < 0) begin
+            A = A + M;
+        end
+        
+        quotient  = Q;
+        remainder = A[n-1:0];
+    end
 endmodule
--- mult_32b.v
+++ mult_32b.v
@@ -1,52 +1,52 @@
 module mult_32b #(parameter n = 32)(
-    input [n-1:0] m,q,
-    output reg [2*n-1:0] result
-);
-//create q prime as a valid wire to be used
-//concatinate q and 0 to make q prime
-//ensures that 0 is one bit, not 32 bits
-wire [n:0] qp = {q,1'b0};
+		 input [n-1:0] m,q,
+		 output reg [2*n-1:0] result
+	);
+	//create q prime as a valid wire to be used
+	//concatinate q and 0 to make q prime
+	//ensures that 0 is one bit, not 32 bits
+	wire [n:0] qp = {q,1'b0};
 
-//this is to store the partial products
-//first parameter is bits of q + m
-//second is bits of q
-reg [2*n-1:0] pp [n-1:0];
+	//this is to store the partial products
+	//first parameter is bits of q + m
+	//second is bits of q
+	reg [2*n-1:0] pp [n-1:0];
 
-//declare integer variable for for-loop
-integer i;
-integer j;
-//create for loop to itereate through qp
-//and assign booth's!
+	//declare integer variable for for-loop
+	integer i, j;
+	//create for loop to itereate through qp
+	//and assign booth's!
 
-//ASSIGNMENT TO COMPLETE
+	//ASSIGNMENT TO COMPLETE
 
-//shift each a bit over using i (like i always would)
-//instead of pp[i], to pp[i>>1], shift operator
-//if and e
-always @ (*) begin
-    for (i=0; i<n; i=i+1) begin
-        if (!qp[i]) begin
-            if (!qp[i+1])
-                pp[i<<1] = 64'b0;
-            else
-                pp[i<<1] = {{32{-m[31]}}, -m};
-        end
-        else begin
-            if (qp[i+1])
-                pp[i<<1] = 64'b0;
-            else
-                pp[i<<1] = {{32{m[31]}}, m};
-        end
-    end
-end
+	//shift each a bit over using i (like i always would)
+	//instead of pp[i], to pp[i>>1], shift operator
+	//if and e
+	always @(*) begin
+		 for (i=0; i<n; i=i+1) begin
+			  if (!qp[i]) begin
+					if (!qp[i+1])
+						 pp[i] = {2*n{1'b0}};
+					else
+						 pp[i] = -{{n{m[n-1]}}, m};
+			  end
+			  else begin
+					if (qp[i+1])
+						 pp[i] = {2*n{1'b0}};
+					else
+						 pp[i] = {{n{m[n-1]}}, m};
+			  end
+			  pp[i] = pp[i] << i;
+		 end
+	end
 
-//adding all of the partial products together to
-//get the final sum
-always @(*) begin
-	result = {2*n{1'b0}};
-    for (j = 0; j<n; j=j+1) begin
-        result = result + pp[j];
-    end
-end
+	//adding all of the partial products together to
+	//get the final sum
+	always @(*) begin
+		 result = {2*n{1'b0}};
+		 for (j=0; j < n; j=j+1) begin
+			  result = result + pp[j];
+		 end
+	end
 
 endmodule
