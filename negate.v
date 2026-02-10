d5def1bfd8fbab89185358737105ad5b1c7dcac0
Flaminiat519 <flaminiatesta5@gmail.com>
2026-02-09 20:03:52 -0500

    Phase1 negate.v

--- negate.v
+++ negate.v
@@ -1,6 +1,9 @@
+//NEGATE operation module
 module negate(
+    //wire initialization
     input [31:0] x,
     output [31:0] z
 );
-assign z = ~x + 1; //Negate operation
+//Negate operation
+assign z = ~x + 1; 
 endmodule
