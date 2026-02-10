5083be9d97d3e3b6b4a47112d9d402f291f6a9cf
duncankeitzke4343 <duncan@keitzke.com>
2026-02-10 09:12:14 -0500

    phase1
    Updated state machine parameters to include new states and corrected formatting.

--- datapath_tb_and.v
+++ datapath_tb_and.v
@@ -16,19 +16,20 @@ module datapath_tb_and;
   wire [31:0] BusMuxOut;
 
   //State machine parameters
-  parameter Default      = 4'b0000,
-            PC_load1a    = 4'b0001,
-            PC_load1b    = 4'b0010,
-            Reg_load1a   = 4'b0011,
-            Reg_load1b   = 4'b0100,
-            Reg_load2a   = 4'b0101,
-            Reg_load2b   = 4'b0110,
-            T0           = 4'b0111,
-            T1           = 4'b1000,
-            T2           = 4'b1001,
-            T3           = 4'b1010,
-            T4           = 4'b1011,
-            T5           = 4'b1100;
+  parameter 
+        Default = 4’b0000, 
+        Reg_load1a = 4’b0001, 
+        Reg_load1b = 4’b0010, 
+        Reg_load2a = 4’b0011,
+        Reg_load2b = 4’b0100, 
+        Reg_load3a = 4’b0101, 
+        Reg_load3b = 4’b0110, 
+        T0 = 4’b0111,
+        T1 = 4’b1000, 
+        T2 = 4’b1001, 
+        T3 = 4’b1010, 
+        T4 = 4’b1011, 
+        T5 = 4’b11
 
   reg [3:0] Present_state = Default;
 
