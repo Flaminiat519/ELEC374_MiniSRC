b35f2a67ce4524cf53c9933f178c0a9cef5a2fea
Flaminiat519 <flaminiatesta5@gmail.com>
2026-02-09 20:33:05 -0500

    Phase1 datapath_tb_not.v

--- datapath_tb_not.v
+++ datapath_tb_not.v
@@ -1,3 +1,4 @@
+//NOT operation test bench
 `timescale 1ns/10ps
 
 module datapath_tb_not;
@@ -64,7 +65,7 @@ module datapath_tb_not;
     .BusMuxOut(BusMuxOut)
   );
 
-  // Clock generation
+  //Clock generation
   initial begin
     clock = 0;
     forever #10 clock = ~clock;
@@ -128,62 +129,43 @@ module datapath_tb_not;
         MDRout <= 1; R7in <= 1;  //Initialize R5 with 52
       end
 
-      /*Reg_load2a: begin
-        deassert_all();
-        MDatain <= 32'd32;
-        Read <= 1; MDRin <= 1;
-      end
-
-      Reg_load2b: begin
-        deassert_all();
-        MDRout <= 1; R6in <= 1;  //Initialize R6 with 32
-      end */
-
       T0: begin
         deassert_all();
-        //FETCH: PCout, MARin, IncPC
+        //Fetch instruction address (fake rn)
         PCout <= 1; MARin <= 1; IncPC <= 1;
       end
 
       T1: begin
         deassert_all();
-        //Read instruction, MDRin
+        //Read instruction
         Read <= 1; MDRin <= 1;
-        MDatain <= 32'h142B0000;   // neg R4, R7
+        MDatain <= 32'h142B0000; //neg R4, R7
       end
 
       T2: begin
         deassert_all();
-        //MDRout, IRin
+        //set IR
         MDRout <= 1; IRin <= 1;
       end
 
       T3: begin
         deassert_all();
-        //EXECUTE NEG: R7out, Yin
         R7out <= 1; Yin <= 1;
       end
 
       T4: begin
         deassert_all();
-        //NEG operation, Zin
+        //NEG operation
         Zin <= 1;
         force DUT.alu_op = (13'b1 << 2);  //NEG index 3
       end
 
       T5: begin
         deassert_all();
-        //Zout, R4in
         Zout <= 1; R4in <= 1;
         release DUT.alu_op;
       end
     endcase
-  end
-
-  //Trace display (used for testing, outputs in the terminal)
-  always @(posedge clock) begin
-    $display("t=%0t state=%b clear=%b | PCin=%b PCout=%b IncPC=%b | Bus=%h | PC=%h Z=%h R5=%h R6=%h R2=%h",
-      $time, Present_state, clear, PCin, PCout, IncPC, BusMuxOut, DUT.PC, DUT.Z, DUT.R5, DUT.R6, DUT.R2);
   end
 
   //Initialize clear signal
