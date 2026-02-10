75c8125728b8f6746c39c054d37cdc4e359d5dd7
Flaminiat519 <flaminiatesta5@gmail.com>
2026-02-09 20:02:59 -0500

    Phase1 not_gate.v

--- not_gate.v
+++ not_gate.v
@@ -1,6 +1,9 @@
+//NOT operation module
 module not_gate(
+    //start wires
     input [31:0] x,
     output [31:0] z
 );
-assign z = ~x; //NOT operation
+//NOT operation
+assign z = ~x; 
 endmodule
