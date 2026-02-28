1161c5d81be06d1b3e49cb70f88d63da89a72439
duncankeitzke4343 <duncan@keitzke.com>
2026-02-28 16:19:04 -0500

    Added various tb

--- Phase 2 Testbenches/jump_instructions_tb.v
+++ Phase 2 Testbenches/jump_instructions_tb.v
@@ -1,9 +1,7 @@
 `timescale 1ns/10ps
-`define ADD 13'b0000000010000
-`define AND 13'b0000000000001
-`define OR 13'b0000000000010
 
-module ALU_immediate_instructions_tb;
+
+module jump_instructions_tb;
 
     reg         Clock, Clear;
     reg         PCin, IRin, HIin, LOin, ZHIin, Zin, MARin, MDRin, OUTPORT_In, Yin;
@@ -24,8 +22,7 @@ module ALU_immediate_instructions_tb;
     // T5  : Zout, Gra, Rin
     parameter Default = 4'b0000;
     parameter T0  = 4'b0001, T0b = 4'b0010, T1  = 4'b0011,
-              T2  = 4'b0100, T3  = 4'b0101, T4  = 4'b0110,
-              T5  = 4'b0111;
+              T2  = 4'b0100, T3  = 4'b0101, T4 = 4'b0110;
 
     reg [3:0] Present_state = Default;
 
@@ -53,15 +50,15 @@ module ALU_immediate_instructions_tb;
     );
 
     initial begin
-        //ADDI instrcution addi R7, R4, -9.
-        //DUT.R4_reg.q = 32'd100;
-        //DUT.PC_reg.qTemp = 32'd6; //instruction located at 0x6 in ram
-		//ANDI instruction
-		//DUT.R4_reg.q = 32'h34;
-        //DUT.PC_reg.qTemp = 32'd8; //instruction located at 0x8 in ram
-		//ORI instruction
-		DUT.R4_reg.q = 32'h34;
-        DUT.PC_reg.qTemp = 32'd9; //instruction located at 0x8 in ram
+        //jump instruction jr R12
+        //DUT.R12_reg.q = 32'hff;
+        //DUT.PC_reg.qTemp = 32'h10; //instruction located at 0x10 in ram
+		
+		//jal instruction jal R4
+		DUT.R4_reg.q = 32'h32;
+		DUT.R12_reg.q = 32'hff;
+        DUT.PC_reg.qTemp = 32'hf; //instruction located at 0x10 in ram
+
 
         Clock = 0;
         forever #10 Clock = ~Clock;
@@ -75,8 +72,7 @@ module ALU_immediate_instructions_tb;
             T0b     : #30 Present_state = T1;
             T1      : #30 Present_state = T2;
             T2      : #30 Present_state = T3;
-            T3      : #30 Present_state = T4;
-            T4      : #30 Present_state = T5;
+			T3		: #30 Present_state = T4;
         endcase
     end
 
@@ -113,24 +109,21 @@ module ALU_immediate_instructions_tb;
 
             // ---- EXECUTE instruction ----
             T3: begin
-                Grb <= 1; Rout <= 1; Yin <= 1;         // Y = R4
-                #40 Grb <= 0; Rout <= 0; Yin <= 0;
+				//jr R12 instruction
+                //Gra <= 1; Rout <= 1; PCin <= 1;         // PC = RA (R12)
+                //#40 Gra <= 0; Rout <= 0; PCin <= 0;
+				//jal R4 instruction
+				Grb <= 1; Rin <= 1; PCout <= 1;				//R12 = PC + 1
+				#40 Grb <= 0; Rin <= 0; PCout <= 0; 
+				
             end
+			
+			//Only for jal R4
+			T4: begin
+				Gra <= 1; Rout <= 1; PCin <= 1;			//PC = R4
+				#40 Gra <= 0; Rout <= 0; PCin <= 0;
+			end
 
-            T4: begin
-                Cout <= 1; 
-				//Comment Out instructions not being tested
-				//alu_op <= `ADD;
-				//alu_op <= `AND;
-				alu_op <= `OR;
-				Zin <= 1;  // Z = Y + C
-                #40 Cout <= 0; Zin <= 0;
-            end
-
-            T5: begin
-                Zout <= 1; Gra <= 1; Rin <= 1;         // R7 = Z
-                #40 Zout <= 0; Gra <= 0; Rin <= 0;
-            end
         endcase
     end
 
