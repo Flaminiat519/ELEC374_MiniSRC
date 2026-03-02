89f2e5bb884ad3d3650f8035eb144fa70621d45a
Flaminiat519 <flaminiatesta5@gmail.com>
2026-03-02 11:58:23 -0500

    Update con_ff.v

--- con_ff.v
+++ con_ff.v
@@ -1,15 +1,16 @@
+//CONDITIONAL LOGIC MODULE
 `timescale 1ns/10ps
 
 module con_ff (
     input wire clk,
     input wire clear,
     input wire CONin,
-    input wire [1:0] C2,          // IR[20:19]
+    input wire [1:0] C2, //IR[20:19]
     input wire [31:0] Bus_Data,
     output reg CON
 );
 
-wire zero     = (Bus_Data == 32'b0);
+wire zero = (Bus_Data == 32'b0);
 wire negative = Bus_Data[31];
 wire positive = (~Bus_Data[31]) & (Bus_Data != 32'b0);
 
@@ -17,10 +18,10 @@ reg condition;
 
 always @(*) begin
     case (C2)
-        2'b00: condition = zero;      // brzr
-        2'b01: condition = ~zero;     // brnz
-        2'b10: condition = positive;  // brpl
-        2'b11: condition = negative;  // brmi
+        2'b00: condition = zero;      //brzr instruction
+        2'b01: condition = ~zero;     //brnz instruction
+        2'b10: condition = positive;  //brpl instruction
+        2'b11: condition = negative;  //brmi instruction
     endcase
 end
 
