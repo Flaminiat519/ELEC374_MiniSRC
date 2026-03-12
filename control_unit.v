8cf107e172843dcc683f2f476fba33123d2ae3c6
Flaminiat519 <flaminiatesta5@gmail.com>
2026-03-12 12:11:23 -0400

    PHASE3

--- control_unit.v
+++ control_unit.v
@@ -1,7 +1,7 @@
 //CONTROL UNIT MODULE
 `timescale 1ns/10ps
 
-//OP code degine statements
+//OP code define statements
 `define ADD 5'b00000
 `define SUB 5'b00001
 `define AND 5'b00010
@@ -33,7 +33,7 @@
 
 //ALU code define statements
 `define AND_OP 13'b0000000000001
-`define OR_OP 13'b0000000000010
+`define OR_OP  13'b0000000000010
 `define NOT_OP 13'b0000000000100
 `define NEG_OP 13'b0000000001000
 `define ADD_OP 13'b0000000010000
@@ -45,226 +45,244 @@
 `define SHRA_OP 13'b0010000000000
 `define ROR_OP 13'b0100000000000
 `define ROL_OP 13'b1000000000000
- 
- 
-module control_unit (
-	//output reg         Clock, Clear;
-    output reg         PCin, IRin, HIin, LOin, ZHIin, Zin, MARin, MDRin, OUTPORT_In, Yin,
-    output reg         PCout, HIout, LOout, ZHIout, Zout, INPORT_Out, MDRout, Cout,
-    output reg         Gra, Grb, Grc, Rin, Rout, BAout, Read, Write, IncPC,
-    output reg         CON_In, CON_Out, OUTPORT_Out,
-	output reg[12:0] alu_op,
-	
-	input [31:0] IR,
-	input Clock, Reset, Stop
-);
-	
-	parameter 	reset_state = 6'b00000, 
-				fetch0 = 6'b00001, 
-				fetch1 = 6'b00010, 
-				fetch2 = 6'b00011,
-				alu3 = 6'b00100, 
-				alu4 = 6'b00101, 
-				alu5 = 6'b00111,
-				muldiv3 = 6'b01001, 
-				muldiv4 = 6'b01010, 
-				muldiv5 = 6'b01011,
-				muldiv6 = 6'b01100, 
-				negnot3 = 6'b01101, 
-				negnot4 = 6'b01110,
-				readwrite3 = 6'b01111, 
-				readwrite4 = 6'b10000,
-				st5 = 6'b10001, 
-				st6 = 6'b10010,
-				st7 = 6'b10011, 
-				st8 = 6'b10100,
-				load5 = 6'b10101, 
-				load6 = 6'b10111,
-				load7 = 6'b11000, 
-				load8 = 6'b11001,
-				alui3 = 6'b11010, 
-				alui4 = 6'b11011, 
-				alui5 = 6'b11100,
-				branch3 = 6'b11101, 
-				branch4 = 6'b11111,
-				jal3 = 6'b100000, 
-				jal4 = 6'b100001,
-				nop = 6'b100010, 
-				halt = 6'b100011,
-				jr3 = 6'b100100,
-				mfhi3 = 6'b100101,
-				mflo3 = 6'b100111,
-				loadi5 = 6'b101000,
-				branch5 = 6'b101001,
-				branch6 = 6'b101011;
- 
-	reg [5:0] present_state = reset_state; 
-	
-	always @(posedge Clock, posedge Reset) // finite state machine; if clock or reset rising-edge
-	begin
-		if (Reset == 1'b1)
-			present_state = reset_state;
-		else 
-			case (present_state)
-				//INSTRUCTION FETCH
-				reset_state: 
-					present_state = fetch0;
-				fetch0: 
-					present_state = fetch1;
-				fetch1: 
-					present_state = fetch2;
-				fetch2: begin
-							case (IR[31:27]) // inst. decoding based on the opcode to set the next state
-								`ADD, `SUB, `AND, `OR, `SHR, `SHRA, `SHL, `ROR, `ROL: present_state = alu3; 
-								`MUL, `DIV: present_state = muldiv3; 
-								`NEG, `NOT: present_state = negnot3; 
-								`ST, `LD, `LDI: present_state = readwrite3; 
-								`ADDI,`ANDI, `ORI: present_state = alui3; 
-								`JR: present_state = jr3; 
-								`JAL: present_state = jal3; 
-								`MFLO: present_state = mflo3; 
-								`MFHI: present_state = mfhi3; 
-								`HALT: present_state = halt;
-								`NOP: present_state = nop;
-								`BRANCH: present_state = branch3;
-							endcase
-						end
-				//ALU OPERATIONS
-				alu3: 
-					present_state = alu4;
-				alu4: 
-					present_state = alu5;
-				alu5:
-					present_state = reset_state;
-					
-				//MUL AND DIV
-				muldiv3: 
-					present_state = muldiv4;
-				muldiv4: 
-					present_state = muldiv5;
-				muldiv5: 
-					present_state = muldiv6;
-				muldiv6:
-					present_state = reset_state;
-			
-				//NEG AND NOT
-				negnot3: 
-					present_state = negnot4;
-				negnot4:
-					present_state = reset_state;
-					
-				//READ AND WRITE
-				readwrite3: 
-					present_state = readwrite4;
-				readwrite4:
-					if (IR[31:27] == `ST)
-						present_state = st5;
-					 else if (IR[31:27] == `LD) 
-						present_state = load5;
-					 else if (IR[31:27] == `LDI) 
-						present_state = loadi5;
-					
-				
-				//STORE
-				st5:
-					present_state = st6;
-				st6:
-					present_state = st7;
-				st7:
-					present_state = st8;
-				st8:
-					present_state = reset_state;
-					
-				//LD
-				load5:
-					present_state = load6;
-				load6:
-					present_state = load7;
-				load7:
-					present_state = load8;
-				load8:
-					present_state = reset_state;
-					
-				//LDI
-				loadi5:
-					present_state = reset_state;
 
-				//MFHI
-				mfhi3:
-					present_state = reset_state;
-					
-				//MFLO
-				mflo3:
-					present_state = reset_state;
-					
-				//ALU IMMEDIATE
-				alui3:
-					present_state = alui4;
-				alui4:
-					present_state = alui5;
-				alui5:
-					present_state = reset_state;
-					
-				//JR
-				jr3:
-					present_state = reset_state;
-				//JAL 
-				jal3:
-					present_state = jal4;
-				jal4:
-					present_state = reset_state;
-					
-				//NOP
-				nop:
-					present_state = reset_state;
-				
-					
-				//BRANCHING
-				branch3:
-					present_state= branch4;
-				branch4:
-					present_state= branch5;
-				branch5:
-					present_state= branch6;
-				branch6:
-					present_state = reset_state;
-					
-				
-				
-			endcase
-	end
-	
-	always @(present_state) // do the job for each state
-	begin
-		// Default everything off
-		PCin=0; IRin=0; HIin=0; LOin=0; ZHIin=0; Zin=0; MARin=0; MDRin=0; OUTPORT_In=0; Yin=0;
-		PCout=0; HIout=0; LOout=0; ZHIout=0; Zout=0; INPORT_Out=0; MDRout=0; Cout=0;
-		Gra=0; Grb=0; Grc=0; Rin=0; Rout=0; BAout=0; Read=0; Write=0; IncPC=0;
-		CON_In=0; CON_Out=0; OUTPORT_Out=0;
-		alu_op=13'b0;
-		case (present_state) // assert the required signals in each state
-			reset_state: begin
-				PCin <=0; IRin <=0; HIin <=0; LOin <=0; ZHIin <=0; Zin <=0; MARin <=0; MDRin <=0; OUTPORT_In <=0; Yin <=0;
-				PCout <=0; HIout <=0; LOout <=0; Zout <=0; ZHIout <=0; INPORT_Out <=0; MDRout <=0; Cout <=0;
-				Gra <=0; Grb <=0; Grc <=0; Rin <=0; Rout <=0; BAout <=0; Read <=0; Write <=0; IncPC <=0;
-				CON_In <=0; CON_Out <=0; OUTPORT_Out <=0;
-			end
-		
-			//INSTRUCTION FETCH!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-			fetch0: begin
-				PCout <= 1; // see if you need to de-assert these signals
-				MARin <= 1;
-				IncPC <= 1;
-				Read <= 1;
-			end
-			fetch1: begin
-				Read <= 1; 
-				MDRin <= 1;
-			end
-			fetch2: begin
-				MDRout <= 1;
-				IRin <= 1;
-			end
+module control_unit (
+
+    output reg PCin, IRin, HIin, LOin, ZHIin, Zin, MARin, MDRin, OUTPORT_In, Yin,
+    output reg PCout, HIout, LOout, ZHIout, Zout, INPORT_Out, MDRout, Cout,
+    output reg Gra, Grb, Grc, Rin, Rout, BAout, Read, Write, IncPC,
+    output reg CON_In, CON_Out, OUTPORT_Out,
+    output reg [12:0] alu_op,
+
+    input [31:0] IR,
+    input Clock, Reset, Stop
+);
+
+parameter  
+    reset_state = 6'd0,
+    fetch0      = 6'd1,
+    fetch1      = 6'd2,
+    fetch2      = 6'd3,
+    fetch3      = 6'd4,
+
+    alu3        = 6'd5,
+    alu4        = 6'd6,
+    alu5        = 6'd7,
+
+    muldiv3     = 6'd8,
+    muldiv4     = 6'd9,
+    muldiv5     = 6'd10,
+    muldiv6     = 6'd11,
+
+    negnot3     = 6'd12,
+    negnot4     = 6'd13,
+
+    readwrite3  = 6'd14,
+    readwrite4  = 6'd15,
+
+    st5         = 6'd16,
+    st6         = 6'd17,
+    st7         = 6'd18,
+    st8         = 6'd19,
+
+    load5       = 6'd20,
+    load6       = 6'd21,
+    load7       = 6'd22,
+    load8       = 6'd23,
+
+    loadi5      = 6'd24,
+
+    alui3       = 6'd25,
+    alui4       = 6'd26,
+    alui5       = 6'd27,
+
+    jr3         = 6'd28,
+    jal3        = 6'd29,
+    jal4        = 6'd30,
+
+    mfhi3       = 6'd31,
+    mflo3       = 6'd32,
+
+    branch3     = 6'd33,
+    branch4     = 6'd34,
+    branch5     = 6'd35,
+    branch6     = 6'd36,
+
+    nop         = 6'd37,
+    halt        = 6'd38;
+
+reg [5:0] present_state;
+
+//
+// FSM STATE TRANSITIONS
+//
+always @(posedge Clock or posedge Reset)
+begin
+    if (Reset)
+        present_state <= reset_state;
+    else
+        case (present_state)
+
+        reset_state: present_state <= fetch0;
+        fetch0:      present_state <= fetch1;
+        fetch1:      present_state <= fetch2;
+        fetch2:      present_state <= fetch3;
+
+        // decode AFTER IR loads
+        fetch3:
+        begin
+            case(IR[31:27])
+
+            `ADD,`SUB,`AND,`OR,`SHR,`SHRA,`SHL,`ROR,`ROL:
+                present_state <= alu3;
+
+            `MUL,`DIV:
+                present_state <= muldiv3;
+
+            `NEG,`NOT:
+                present_state <= negnot3;
+
+            `ST,`LD,`LDI:
+                present_state <= readwrite3;
+
+            `ADDI,`ANDI,`ORI:
+                present_state <= alui3;
+
+            `JR:
+                present_state <= jr3;
+
+            `JAL:
+                present_state <= jal3;
+
+            `MFHI:
+                present_state <= mfhi3;
+
+            `MFLO:
+                present_state <= mflo3;
+
+            `BRANCH:
+                present_state <= branch3;
+
+            `HALT:
+                present_state <= halt;
+
+            `NOP:
+                present_state <= nop;
+
+            default:
+                present_state <= fetch0;
+
+            endcase
+        end
+
+        // ALU
+        alu3: present_state <= alu4;
+        alu4: present_state <= alu5;
+        alu5: present_state <= reset_state;
+
+        // MUL DIV
+        muldiv3: present_state <= muldiv4;
+        muldiv4: present_state <= muldiv5;
+        muldiv5: present_state <= muldiv6;
+        muldiv6: present_state <= reset_state;
+
+        // NEG NOT
+        negnot3: present_state <= negnot4;
+        negnot4: present_state <= reset_state;
+
+        // LOAD STORE
+        readwrite3: present_state <= readwrite4;
+
+        readwrite4:
+        begin
+            if (IR[31:27] == `ST)
+                present_state <= st5;
+            else if (IR[31:27] == `LD)
+                present_state <= load5;
+            else
+                present_state <= loadi5;
+        end
+
+        load5: present_state <= load6;
+        load6: present_state <= load7;
+        load7: present_state <= load8;
+        load8: present_state <= reset_state;
+
+        loadi5: present_state <= reset_state;
+
+        st5: present_state <= st6;
+        st6: present_state <= st7;
+        st7: present_state <= st8;
+        st8: present_state <= reset_state;
+
+        // ALU IMMEDIATE
+        alui3: present_state <= alui4;
+        alui4: present_state <= alui5;
+        alui5: present_state <= reset_state;
+
+        // JUMP
+        jr3: present_state <= reset_state;
+
+        jal3: present_state <= jal4;
+        jal4: present_state <= reset_state;
+
+        mfhi3: present_state <= reset_state;
+        mflo3: present_state <= reset_state;
+
+        branch3: present_state <= branch4;
+        branch4: present_state <= branch5;
+        branch5: present_state <= branch6;
+        branch6: present_state <= reset_state;
+
+        nop: present_state <= reset_state;
+        halt: present_state <= halt;
+
+        endcase
+end
+
+
+//
+// CONTROL SIGNALS
+//
+always @(present_state)
+begin
+
+// default everything OFF
+PCin=0; IRin=0; HIin=0; LOin=0; ZHIin=0; Zin=0; MARin=0; MDRin=0; OUTPORT_In=0; Yin=0;
+PCout=0; HIout=0; LOout=0; ZHIout=0; Zout=0; INPORT_Out=0; MDRout=0; Cout=0;
+Gra=0; Grb=0; Grc=0; Rin=0; Rout=0; BAout=0; Read=0; Write=0; IncPC=0;
+CON_In=0; CON_Out=0; OUTPORT_Out=0;
+alu_op=0;
+
+case(present_state)
+
+reset_state: begin end
+
+// FETCH
+fetch0:
+begin
+PCout=1;
+MARin=1;
+IncPC=1;
+Read=1;
+end
+
+fetch1:
+begin
+Read=1;
+MDRin=1;
+end
+
+fetch2:
+begin
+MDRout=1;
+IRin=1;
+end
+
+fetch3:
+begin
+// decode only
+end
 		
 			//ADD, AND, OR, SUB, SHR, SHL, SHRA, ROL, ROR
 			alu3: begin
@@ -481,3 +499,6 @@ module control_unit (
 		endcase
 	end
 	endmodule
+	
+	
+	
--- cpu_tb.v
+++ cpu_tb.v
@@ -2,239 +2,96 @@
 
 module cpu_tb;
 
-    reg        Clock;
-    reg        Reset;
-    reg        Stop;
-    wire [31:0] BusMuxOut;
+reg Clock;
+reg Reset;
+reg Stop;
 
-    // Instantiate CPU (no MDatain needed)
-    CPU DUT (
-        .Clock(Clock),
-        .Reset(Reset),
-        .Stop(Stop),
-        .BusMuxOut(BusMuxOut)
-    );
+wire [31:0] BusMuxOut;
 
-    // 10ns clock
-    initial Clock = 0;
-    always #5 Clock = ~Clock;
+// Instantiate CPU
+CPU dut(
+    .Clock(Clock),
+    .Reset(Reset),
+    .Stop(Stop),
+    .BusMuxOut(BusMuxOut)
+);
 
-    // Waveform dump
-    initial begin
-        $dumpfile("cpu_tb.vcd");
-        $dumpvars(0, cpu_tb);
+//
+// CLOCK GENERATOR
+//
+initial begin
+    Clock = 0;
+    forever #10 Clock = ~Clock;
+end
+
+//
+// MAIN TEST
+//
+initial begin
+
+    $display("=====================================");
+    $display("      MiniSRC Phase 3 Simulation     ");
+    $display("=====================================");
+
+    Stop = 0;
+    Reset = 1;
+
+    #50;
+    Reset = 0;
+
+    // allow program to run
+    #60000;
+
+    $display("Simulation timeout reached.");
+    $stop;
+
+end
+
+
+//
+// INSTRUCTION TRACE
+//
+always @(posedge Clock) begin
+
+    if(!Reset) begin
+
+        $display(
+        "Time=%0t | PC=%h | IR=%h | BUS=%h | STATE=%0d",
+        $time,
+        dut.dp.PC,
+        dut.dp.IR,
+        BusMuxOut,
+        dut.con.present_state
+        );
+
     end
 
-    // -------------------------------------------------------
-    // Helper: wait N clock cycles then print state
-    // -------------------------------------------------------
-    task wait_cycles;
-        input integer n;
-        integer i;
-        begin
-            for (i = 0; i < n; i = i + 1)
-                @(posedge Clock);
-            #1; // small delta after edge to read stable values
-        end
-    endtask
+end
 
-    // -------------------------------------------------------
-    // Helper: print current CPU state
-    // -------------------------------------------------------
-    task print_state;
-        input [255:0] label;
-        begin
-            $display("[%0t] %s", $time, label);
-            $display("  PC=%h  IR=%h  Bus=%h",
-                DUT.dp.PC,
-                DUT.dp.IR,
-                BusMuxOut);
-            $display("  R0=%h R1=%h R2=%h R3=%h",
-                DUT.dp.R0, DUT.dp.R1, DUT.dp.R2, DUT.dp.R3);
-            $display("  R4=%h R5=%h R6=%h R7=%h",
-                DUT.dp.R4, DUT.dp.R5, DUT.dp.R6, DUT.dp.R7);
-            $display("  HI=%h  LO=%h",
-                DUT.dp.HI, DUT.dp.LO);
-            $display("");
-        end
-    endtask
 
-    // -------------------------------------------------------
-    // Reset task
-    // -------------------------------------------------------
-    task do_reset;
-        begin
-            Reset = 1; Stop = 0;
-            @(posedge Clock); #1;
-            Reset = 0;
-            $display("[%0t] === Reset Released ===\n", $time);
-        end
-    endtask
+//
+// HALT DETECTOR
+//
+always @(posedge Clock) begin
 
-    // -------------------------------------------------------
-    // Main test
-    // -------------------------------------------------------
-    // Cycle counts per instruction type (fetch=3 + execute):
-    //   LDI:   3+5 = 8  cycles
-    //   LD:    3+8 = 11 cycles
-    //   ST:    3+8 = 11 cycles
-    //   ADDI:  3+5 = 8  cycles
-    //   ANDI:  3+5 = 8  cycles
-    //   ORI:   3+5 = 8  cycles
-    //   BRANCH:3+6 = 9  cycles
-    //   MFHI:  3+3 = 6  cycles
-    //   MFLO:  3+3 = 6  cycles
-    //   JAL:   3+4 = 7  cycles
-    //   JR:    3+1 = 4  cycles
-    //   IN/OUT:3+? = ~8 cycles (check your CU states)
-    // -------------------------------------------------------
-
-    initial begin
-        $display("======================================");
-        $display("        CPU Testbench Start           ");
-        $display("======================================\n");
-
-        do_reset;
-
-        // --------------------------------------------------
-        // @000: ldi R7, 0x65
-        // R7 should become 0x00000065
-        // --------------------------------------------------
-        wait_cycles(8);
-        print_state("@000 LDI R7, 0x65 -- expect R7=0x00000065");
-
-        // --------------------------------------------------
-        // @001: ldi R0, 0x72(R2)  (R2=0, so addr=0x72, R0=sign_ext(0x72))
-        // R0 should become 0x00000072
-        // --------------------------------------------------
-        wait_cycles(8);
-        print_state("@001 LDI R0, 0x72(R2) -- expect R0=0x00000072");
-
-        // --------------------------------------------------
-        // @002: ld R7, 0x65  (loads mem[0x65]=0x84 into R7)
-        // R7 should become 0x00000084
-        // --------------------------------------------------
-        wait_cycles(11);
-        print_state("@002 LD R7, 0x65 -- expect R7=0x00000084");
-
-        // --------------------------------------------------
-        // @003: ld R0, 0x72(R2)  (R2=0, mem[0x72] -- check what's there)
-        // R0 = mem[0x72] (likely 0 unless written)
-        // --------------------------------------------------
-        wait_cycles(11);
-        print_state("@003 LD R0, 0x72(R2) -- expect R0=mem[0x72]");
-
-        // --------------------------------------------------
-        // @004: st 0x1F, R6  (store R6 into mem[0x1F])
-        // mem[0x1F] was 0xD4, now should be R6's value (0 after reset)
-        // --------------------------------------------------
-        wait_cycles(11);
-        print_state("@004 ST 0x1F, R6 -- mem[0x1F] should now = R6");
-        $display("  mem[0x1F] = %h (expect R6 value)\n",
-            DUT.dp.RAM.mem[8'h1F]);
-
-        // --------------------------------------------------
-        // @005: st 0x1F(R6), R6  (R6=0, so same address 0x1F)
-        // --------------------------------------------------
-        wait_cycles(11);
-        print_state("@005 ST 0x1F(R6), R6 -- mem[0x1F] = R6 again");
-        $display("  mem[0x1F] = %h\n", DUT.dp.RAM.mem[8'h1F]);
-
-        // --------------------------------------------------
-        // @006: addi R7, R4, -9  (R4=0, so R7 = 0 + (-9) = 0xFFFFFFF7)
-        // --------------------------------------------------
-        wait_cycles(8);
-        print_state("@006 ADDI R7, R4, -9 -- expect R7=0xFFFFFFF7");
-
-        // --------------------------------------------------
-        // @007: addi R7, R4, 0x71  (R4=0, R7 = 0x71)
-        // --------------------------------------------------
-        wait_cycles(8);
-        print_state("@007 ADDI R7, R4, 0x71 -- expect R7=0x00000071");
-
-        // --------------------------------------------------
-        // @008: andi R7, R4, 0x71  (R4=0, R7 = 0 & 0x71 = 0)
-        // --------------------------------------------------
-        wait_cycles(8);
-        print_state("@008 ANDI R7, R4, 0x71 -- expect R7=0x00000000");
-
-        // --------------------------------------------------
-        // @009: ori R7, R4, 0x71  (R4=0, R7 = 0 | 0x71 = 0x71)
-        // --------------------------------------------------
-        wait_cycles(8);
-        print_state("@009 ORI R7, R4, 0x71 -- expect R7=0x00000071");
-
-        // --------------------------------------------------
-        // @00A: brzr R3, 48  (R3=0 so branch taken, PC += 48)
-        // --------------------------------------------------
-        wait_cycles(9);
-        print_state("@00A BRZR R3, 48 -- R3=0 so branch TAKEN, expect PC jump");
-
-        // --------------------------------------------------
-        // @00B: brnz R3, 48  (R3=0 so branch NOT taken)
-        // --------------------------------------------------
-        wait_cycles(9);
-        print_state("@00B BRNZ R3, 48 -- R3=0 so branch NOT taken");
-
-        // --------------------------------------------------
-        // @00C: brpl R3, 48  (R3=0, not positive so NOT taken)
-        // --------------------------------------------------
-        wait_cycles(9);
-        print_state("@00C BRPL R3, 48 -- R3=0 not positive, NOT taken");
-
-        // --------------------------------------------------
-        // @00D: brmi R3, 48  (R3=0, not negative so NOT taken)
-        // --------------------------------------------------
-        wait_cycles(9);
-        print_state("@00D BRMI R3, 48 -- R3=0 not negative, NOT taken");
-
-        // --------------------------------------------------
-        // @00E: mfhi R5  (R5 = HI register)
-        // --------------------------------------------------
-        wait_cycles(6);
-        print_state("@00E MFHI R5 -- expect R5=HI");
-
-        // --------------------------------------------------
-        // @00F: jal R4  (R15=PC, PC=R4)
-        // --------------------------------------------------
-        wait_cycles(7);
-        print_state("@00F JAL R4 -- expect R15=return addr, PC=R4(=0)");
-
-        // --------------------------------------------------
-        // @010: jr R12  (PC = R12, likely 0 after reset)
-        // --------------------------------------------------
-        wait_cycles(4);
-        print_state("@010 JR R12 -- expect PC=R12");
-
-        // --------------------------------------------------
-        // @011: mflo R1  (R1 = LO register)
-        // --------------------------------------------------
-        wait_cycles(6);
-        print_state("@011 MFLO R1 -- expect R1=LO");
-
-        // --------------------------------------------------
-        // @012: in R5
-        // --------------------------------------------------
-        wait_cycles(8);
-        print_state("@012 IN R5");
-
-        // --------------------------------------------------
-        // @013: out R7
-        // --------------------------------------------------
-        wait_cycles(8);
-        print_state("@013 OUT R7");
-
-        $display("======================================");
-        $display("        Testbench Complete            ");
-        $display("======================================");
-        $finish;
+    if(dut.dp.IR == 32'hD8000000) begin
+        $display("HALT instruction detected at time %0t", $time);
+        $display("Program execution completed.");
+        #50;
+        $stop;
     end
 
-    // Watchdog
-    initial begin
-        #50000;
-        $display("TIMEOUT - simulation ran too long");
-        $finish;
-    end
+end
+
+
+//
+// WAVEFORM OUTPUT
+//
+initial begin
+
+    $dumpfile("cpu.vcd");
+    $dumpvars(0, cpu_tb);
+
+end
 
 endmodule
