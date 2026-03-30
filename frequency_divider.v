518d0239caa05603dbafc33a44bbe7b5255951a1
duncankeitzke4343 <duncan@keitzke.com>
2026-03-30 17:33:55 -0400

    commit

--- frequency_divider.v
+++ frequency_divider.v
@@ -7,14 +7,14 @@ module frequency_divider(
 	output reg div_clk
 );
 
-	reg [2:0] counter;
+	reg [31:0] counter;
 	
 	always @(posedge CLOCK_50 or posedge reset) begin
 		if(reset) begin
 			counter <= 0;
 			div_clk <= 0;
 		end else begin
-			if(counter == 3'd3) begin
+			if(counter == 3'd500) begin
 				div_clk <= ~div_clk;
 				counter <= 0;
 			end else begin
