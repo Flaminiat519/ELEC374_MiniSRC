3e2ab662131b1371247db0721637e79fa80d23fe
Flaminiat519 <flaminiatesta5@gmail.com>
2026-06-06 16:47:35 -0400

    Clean sll.v

--- sll.v
+++ sll.v
@@ -1,9 +1,7 @@
-//Shift left module
+//32-bit logical shift left
 module sll (
-	//wire initialization
 	input wire [31:0] a, b,
 	output wire [31:0] z
 );
-	//shift left operation
 	assign z = a << b;
 endmodule
