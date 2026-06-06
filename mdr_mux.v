ef703c94f6acddbda020a822660f54734e84383d
Flaminiat519 <flaminiatesta5@gmail.com>
2026-06-06 16:30:45 -0400

    Clean mdr_mux.v

--- mdr_mux.v
+++ mdr_mux.v
@@ -1,16 +1,15 @@
-//MDR multiplexer module
+//MDR multiplexer — selects between memory data and bus data depending on Read
 `timescale 1ns/10ps
-
 module mdr_mux (
-    input wire [31:0] BusMuxOut,
-    input wire Read,
-    input wire [31:0] MDatain,
-    output reg [31:0] Q
+	input wire [31:0] BusMuxOut,
+	input wire Read,
+	input wire [31:0] MDatain,
+	output reg [31:0] Q
 );
-    always @(*) begin
-        if (Read) 
-            Q = MDatain;
-        else
-            Q = BusMuxOut;
-    end
+	always @(*) begin
+		if (Read)
+			Q = MDatain; //Load from memory
+		else
+			Q = BusMuxOut; //Load from bus
+	end
 endmodule
