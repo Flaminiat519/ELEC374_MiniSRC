86f799a9b48c6ee76e80bec2ab71cc7aee28afb3
Flaminiat519 <flaminiatesta5@gmail.com>
2026-06-06 16:39:44 -0400

    Clean register.v

--- register.v
+++ register.v
@@ -1,25 +1,22 @@
-//General Register module
+//General-purpose register — latches input from the bus when enabled
 module register #(
-  parameter DATA_WIDTH_IN  = 32,
-  parameter DATA_WIDTH_OUT = 32,
-  parameter INIT           = 32'b0
+	parameter DATA_WIDTH_IN  = 32,
+	parameter DATA_WIDTH_OUT = 32,
+	parameter INIT           = 32'b0
 )(
-  input wire clear,
-  input wire clock,
-  input wire enable,
-  input wire [DATA_WIDTH_IN-1:0]  BusMuxIn, //input?
-  output wire [DATA_WIDTH_OUT-1:0] BusMuxOut //output?
+	input wire clear,
+	input wire clock,
+	input wire enable,
+	input wire  [DATA_WIDTH_IN-1:0]  BusMuxIn,
+	output wire [DATA_WIDTH_OUT-1:0] BusMuxOut
 );
+	reg [DATA_WIDTH_IN-1:0] q;
+	initial q = INIT;
 
-  reg [DATA_WIDTH_IN-1:0] q;
-  initial q = INIT;
+	always @(posedge clock) begin
+    if (clear) q <= {DATA_WIDTH_IN{1'b0}}; //Synchronous reset
+    else if (enable) q <= BusMuxIn; //Latch bus value
+	end
 
-  always @(posedge clock) begin
-    if (clear)       q <= {DATA_WIDTH_IN{1'b0}};
-    else if (enable) q <= BusMuxIn;
-  end
-
-  assign BusMuxOut = q[DATA_WIDTH_OUT-1:0];
-
+	assign BusMuxOut = q[DATA_WIDTH_OUT-1:0];
 endmodule
-
