5d0c3da21d37cc1199fcb091bb4c05e34daad534
Flaminiat519 <flaminiatesta5@gmail.com>
2026-06-06 16:12:11 -0400

    Clean and_gate.v

--- and_gate.v
+++ and_gate.v
@@ -1,10 +1,8 @@
-//32-bit and gate module
+// 32-bit bitwise AND gate
 module and_gate (
-    //define 32-bit inputs and output
-    input [31:0] x,y,
-    output [31:0] z
+	input [31:0] x, y,
+	output [31:0] z
 );
-//AND operation
-assign z = x & y; 
-    
-endmodule 
+	assign z = x & y;
+
+endmodule
