f8216dbc8152583a919da116f0661ac9fa27d0fe
Flaminiat519 <flaminiatesta5@gmail.com>
2026-06-06 16:41:40 -0400

    Clean ror.v

--- ror.v
+++ ror.v
@@ -1,14 +1,14 @@
-//Rotate right module
+//32-bit rotate right
 module ror (
-	//wire initialization
 	input wire [31:0] a, b,
 	output wire [31:0] z
 );
-	//rotate right operation
 	reg [31:0] temp;
-	always @ (*)
-		begin
-			temp = ((a >> b) | (a << (32 - b)));
-		end
+
+	always @(*) begin
+		//Rotate a right by b positions
+		temp = (a >> b) | (a << (32 - b));
+	end
+
 	assign z = temp;
 endmodule
