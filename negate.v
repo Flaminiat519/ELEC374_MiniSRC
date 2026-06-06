db36b7a182dbcdac878ce9e445fd8855cf62ebcd
Flaminiat519 <flaminiatesta5@gmail.com>
2026-06-06 16:34:17 -0400

    Clean negate.v

--- negate.v
+++ negate.v
@@ -1,9 +1,7 @@
-//NEGATE operation module
+//32-bit negation — computes the two's complement of the input
 module negate(
-    //wire initialization
-    input [31:0] x,
-    output [31:0] z
+	input [31:0] x,
+	output [31:0] z
 );
-//Negate operation
-assign z = ~x + 1; 
+	assign z = ~x + 1;
 endmodule
