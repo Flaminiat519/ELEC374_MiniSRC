6d79bc29fad668a1e6459316d6213fe0268ea37e
Flaminiat519 <flaminiatesta5@gmail.com>
2026-06-06 16:35:16 -0400

    Clean or_gate.v

--- or_gate.v
+++ or_gate.v
@@ -1,9 +1,7 @@
-//OR gate module
+//32-bit bitwise OR gate
 module or_gate(
-    //start wires
-    input [31:0] x,y,
-    output [31:0] z
+	input [31:0] x, y,
+	output [31:0] z
 );
-    //OR operation
-    assign z = x|y; 
+	assign z = x | y;
 endmodule
